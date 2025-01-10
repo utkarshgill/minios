2dfdef6b3c90ab254db20da661ed61fc5282bcf2
Utkarsh Gill <engelbart@Utkarshs-MacBook-Pro.local>
2025-01-10 13:27:05 +0530

    printf has entered the chat

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ QEMU=qemu-system-riscv32
 
 # path to cland and compiler flags
 CC=/opt/homebrew/opt/llvm/bin/clang
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 --ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 # build the kernel
-$CC $CFLAGS -W1, -Tkernel.ld -W1, -Map=kernel.map -o kernel.elf \
-    kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c
 
 # start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \

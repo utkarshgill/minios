5a54dcedbdb4f696d6d0b9e692e6798390e87b30
Utkarsh Gill <engelbart@Utkarshs-MacBook-Pro.local>
2025-01-10 11:46:56 +0530

    linker and kernel

---
+++ run.sh
@@ -0,0 +1,17 @@
+/#!bin/bash
+set -xue
+
+# QEMU file path
+QEMU=qemu-system-riscv32
+
+# path to cland and compiler flags
+CC=/opt/homebrew/opt/llvm/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 --ffreestanding -nostdlib"
+
+# build the kernel
+$CC $CFLAGS -W1, -Tkernel.ld -W1, -Map=kernel.map -o kernel.elf \
+    kernel.c
+
+# start QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
